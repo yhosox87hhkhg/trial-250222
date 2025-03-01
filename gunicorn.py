3cc1c3d53e704f41608605c5f7541670c5bce8bd
hosox <hosox@f40-trial-000.local.hosox.net>
2025-03-01 16:05:50 +0900

    change worker

--- gunicorn.py
+++ gunicorn.py
@@ -6,4 +6,6 @@ name = "Gunicorn config for FastAPI"
 bind = "0.0.0.0:8000"
 
 worker_class = "uvicorn.workers.UvicornWorker"
-workers = multiprocessing.cpu_count () * 2 + 1
+workers = 1
+timeout = 120
+keepalive = 5
